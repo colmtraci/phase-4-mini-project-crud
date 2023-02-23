57b2a03ecb2e7d36cf57ad6049bbdaed8f35a97e
Colm Traci <ctraci@me.com>
2023-02-23 15:34:54 -0800

    commit tree adf4a46f95ccc471865a020655ccae13a6d52df2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # GET '/spices', to: 'spices#index'
+  # POST '/spices' to: 'spices#create'
+  # PATCH '/spices/:id' to: 'spices#update'
+  # DELETE '/spices/:id' to: 'spices#destroy'
+  
+  resources :spices, only: [:index, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
